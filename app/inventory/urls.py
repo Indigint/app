8d4f26ea6d0b9df62330a7b9722d1c62b28e7f6d
i2c2-caj <i2c2.caj@gmail.com>
2015-03-12 14:32:05 -0600

    started adding text content into the website.

--- app/inventory/urls.py
+++ app/inventory/urls.py
@@ -4,7 +4,7 @@ from inventory import views
 
 urlpatterns = patterns('',
                        # /po
-                       url(r'^$', views.index, name='index'),
+                       url(r'^$', views.index, name='po_index'),
                        # /po/X/
-                       url(r'^(?P<PO_id>\d+)/$', views.po, name='po'),
+                       url(r'^(?P<PO_id>\d+)/$', views.po, name='po_view'),
                       )
